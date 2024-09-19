d10ddaf936806faf405620d6d5829b4df8a5e1a2
TahaBh <tahahasanbhatti@icloud.com>
2024-09-18 17:50:06 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Featured from './components/Featured'
+import Navbar from './components/Navbar'
+import Signup from './components/Signup'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+
+
+function App() {
+  return (
+    <>
+      
+   <Navbar />
+   <Featured />
+   <Hero />
+   <Signup />
+   <Footer />
+
+    </>
+  );
+}
+
+export default App;
